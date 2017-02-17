968cfcc622b2b16e38bcdc9a9b97203f9a2ae12b
Your Name <you@example.com>
2017-02-17 16:39:44 +0800

    homework

---
+++ MyFirstWinForm/MyData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWinForm
+{
+    class MyData
+    {
+        private int _x;
+        private int _y;
+        
+        public int x
+        {
+            get { return _x;}
+            set { _x = value;}
+           
+        }
+        public int x
+        {
+            get { return _y; }
+            set { _y = value;}
+        }
+    }
+}
